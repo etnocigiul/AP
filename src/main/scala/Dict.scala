8c2be25b338c31d6461f770dcef89f4afca5c888
etnocigiul <etnocigiul@gmail.com>
2017-03-31 09:55:26 +0200

    import source file and building

--- src/main/scala/Dict.scala
+++ src/main/scala/Dict.scala
@@ -1,40 +1,40 @@
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
-import org.apache.spark.SparkConf
-import org.apache.spark.HashPartitioner
-import org.apache.spark.rdd._
- 
-object Dict {
 
-	private val MAXINT = 5000000
+object Dict{
+
+	private val MAXINT = 100000
+
 	def main(args: Array[String]) {
+		
 		val inputFile = "/home/bijo/Prove/Dict/sentences.txt"
-
-		//Inizializzo lo SparkContext
 		val conf = new SparkConf().setAppName("Dict")
 		val sc = new SparkContext(conf)
 
-		//Carico l'input
 		val input = sc.textFile(inputFile)
+		val dict = input.flatMap(line => line.split("[^\\w]+")).filter(v => v.length > 1).map(x => (x, 1)).reduceByKey((x, y) => x + y).sortByKey()
 
-		//Creo un dizionario dall'input. In questa Map gli elementi sono del tipo (Parola, n°occorrenze)
-		val dict = input.flatMap(line => line.split("\\P{L}+")).map(x => (x, 1)).reduceByKey((x, y) => x + y).sortByKey().partitionBy(new HashPartitioner(100)).persist()
+		val numbers = dict.map(v => v._2.toDouble).filter(x => x > 1).distinct()
 
-		//Creo un RDD contenente solo i valori superiori a 1
-		val numbers = dict.map(k => k._2.toDouble).filter(x => x > 1).distinct()
+		val rm = recursiveMap(numbers)D
 
-		val rm = recursiveMap(numbers)
-
-		println("|||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||")
-		dict.foreach(v => println(v._1 + ": \t" + v._2))
-		println("|||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||")
+		println("|||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||")
+		dict.foreach(v => println(v._1 + ": \t\t" + v._2))
+		println("|||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||")
 		numbers.foreach(println(_))
-		println("|||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||")
-		println("Il max è: " + rm.max + ", superiore di " + (rm.max - MAXINT) + " rispetto a MAXINT")
+		println("|||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||")
+		rm.foreach(println(_))
+		println("|||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||")
+		
+
+		val max = numbers.max
+		dict.filter( _._2 == max).foreach(v => println(v._1))
+		println("|||||||||||||||||||||||||||||||||||||||||||||||\n|||||||||||||||||||||||||||||||||||||||||||||||")
+
 		}
 
-	// Funzione map ricorsiva
 	def recursiveMap(num : RDD[Double]) : RDD[Double] = if(num.max < MAXINT) recursiveMap(num.map(x => x*x)) else num
-
 
 }
